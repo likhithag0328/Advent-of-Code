f28b02e03f5360bb97f0dadd853f1a4641bb5d9e
likhi_g <183710719+likhithag0328@users.noreply.github.com>
2025-02-02 01:20:09 +0530

    day-3 part 2

--- 2019/day-3/crossedWires.js
+++ 2019/day-3/crossedWires.js
@@ -1,68 +1,58 @@
-const parse = (path) => {
-  return { direction: path[0], steps: parseInt(path.slice(1), 10) };
-};
+const parse = (path) => ({
+  direction: path[0],
+  steps: parseInt(path.slice(1), 10),
+});
 
 const findPath = (wire) => {
-  const centralPort = [0, 0];
-  let [i, j] = centralPort;
+  let [x, y] = [0, 0];
   const path = [];
 
   wire.forEach((point) => {
     const { direction, steps } = parse(point);
-
     for (let step = 0; step < steps; step++) {
-      if (direction === "R") j += 1;
-      if (direction === "L") j -= 1;
-      if (direction === "U") i -= 1;
-      if (direction === "D") i += 1;
-      path.push([i, j]);
+      if (direction === "R") x += 1;
+      if (direction === "L") x -= 1;
+      if (direction === "U") y -= 1;
+      if (direction === "D") y += 1;
+      path.push([x, y]);
     }
   });
 
   return path;
 };
 
-const exists = (set, subArray) => set.has(subArray.join(","));
+const index = (array, subArray) =>
+  array.findIndex((point) =>
+    point[0] === subArray[0] && point[1] === subArray[1]
+  );
 
-const intersection = (pathWire1, wire2) => {
-  const centralPort = [0, 0];
-  let [i, j] = centralPort;
-  const intersectingPoints = [];
+const findSteps = (wire1Paths, wire2Paths, intersections) =>
+  intersections.map((point) =>
+    index(wire1Paths, point) + index(wire2Paths, point) + 2
+  );
 
-  // Convert pathWire1 into a Set for O(1) lookups
-  const pathWire1Set = new Set(pathWire1.map((point) => point.join(",")));
+const allIntersections = (wire1Paths, wire2Paths) => {
+  const pathWire1Set = new Set(wire1Paths.map((point) => point.join(",")));
+  const pathWire2Set = new Set(wire2Paths.map((point) => point.join(",")));
+  const intersections = Array.from(pathWire1Set.intersection(pathWire2Set)).map(
+    (point) => point.split(",").map(Number),
+  );
 
-  wire2.forEach((point) => {
-    const { direction, steps } = parse(point);
-
-    for (let step = 0; step < steps; step++) {
-      if (direction === "R") j += 1;
-      if (direction === "L") j -= 1;
-      if (direction === "U") i -= 1;
-      if (direction === "D") i += 1;
-
-      if (exists(pathWire1Set, [i, j])) {
-        intersectingPoints.push([i, j]);
-      }
-    }
-  });
-
-  return intersectingPoints;
+  return findSteps(
+    wire1Paths,
+    wire2Paths,
+    intersections,
+  );
 };
 
 function solve() {
   const [input1, input2] = Deno.readTextFileSync("./day-3/input.txt")
     .split("\n");
-  const wire1 = input1.split(",");
-  const wire2 = input2.split(",");
-  const pathWire1 = findPath(wire1);
-  const intersectingPoints = intersection(pathWire1, wire2);
-  const distances = intersectingPoints.map((arr) =>
-    Math.abs(arr[0]) + Math.abs(arr[1])
-  );
-  console.log(distances.sort((a, b) => a - b)[0]);
+  const pathWire1 = findPath(input1.split(","));
+  const pathWire2 = findPath(input2.split(","));
+  return allIntersections(pathWire1, pathWire2).sort((a, b) => a - b)[0];
 }
 
 console.time();
-solve();
+console.log(solve());
 console.timeEnd();
