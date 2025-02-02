4460d0ffd88e3e614922cb9bce47228d4f7b6bb6
likhi_g <183710719+likhithag0328@users.noreply.github.com>
2025-02-02 18:31:28 +0530

    day-5 part 2

--- 2019/day-5/intCode.js
+++ 2019/day-5/intCode.js
@@ -1,44 +1,66 @@
 class Interpret {
   #code;
-  #temp;
   constructor(data) {
-    this.#code = data;
+    this.#code = [...data]; // Copy data to avoid modifying original input
   }
 
-  #parseArgs(index) {
-    const modes = parseInt(this.#code[index] / 100);
-    const operand1 = modes % 10 === 1
-      ? this.#code[++index]
-      : this.#code[this.#code[++index]];
-    const operand2 = parseInt(modes / 10) === 1
-      ? this.#code[++index]
-      : this.#code[this.#code[++index]];
-    return [operand1, operand2, this.#code[++index]];
+  #parseArgs(index, count) {
+    const modes = Math.floor(this.#code[index] / 100);
+    const args = [];
+
+    for (let i = 0; i < count; i++) {
+      args.push(
+        Math.floor(modes / Math.pow(10, i)) % 10 === 1
+          ? this.#code[++index]
+          : this.#code[this.#code[++index]],
+      );
+    }
+
+    return args;
   }
-  // #parseArgs(index) {
-  //   return [
-  //     this.#code[this.#code[++index]],
-  //     this.#code[this.#code[++index]],
-  //     this.#code[++index],
-  //   ];
-  // }
 
   #add(index) {
-    const [addend, augend, resultIndex] = this.#parseArgs(index);
+    const [addend, augend] = this.#parseArgs(index, 2);
+    const resultIndex = this.#code[index + 3];
     this.#code[resultIndex] = addend + augend;
   }
 
   #multiply(index) {
-    const [multiplicand, multiplier, resultIndex] = this.#parseArgs(index);
+    const [multiplicand, multiplier] = this.#parseArgs(index, 2);
+    const resultIndex = this.#code[index + 3];
     this.#code[resultIndex] = multiplicand * multiplier;
   }
 
   #set(index, ID) {
-    this.#code[this.#code[++index]] = ID;
+    const resultIndex = this.#code[index + 1];
+    this.#code[resultIndex] = ID;
   }
 
   #get(index) {
-    console.log(this.#code[this.#code[++index]]);
+    const [value] = this.#parseArgs(index, 1);
+    console.log(value);
+  }
+
+  #jumpIfTrue(index) {
+    const [operand, nextIndex] = this.#parseArgs(index, 2);
+    return operand !== 0 ? nextIndex : index + 3;
+  }
+
+  #jumpIfFalse(index) {
+    const [operand, nextIndex] = this.#parseArgs(index, 2);
+    return operand === 0 ? nextIndex : index + 3;
+  }
+
+  #isLessThan(index) {
+    const [operand1, operand2] = this.#parseArgs(index, 2);
+    const resultIndex = this.#code[index + 3];
+    this.#code[resultIndex] = operand1 < operand2 ? 1 : 0;
+  }
+
+  #isEquals(index) {
+    const [operand1, operand2] = this.#parseArgs(index, 2);
+    const resultIndex = this.#code[index + 3];
+    this.#code[resultIndex] = operand1 === operand2 ? 1 : 0;
   }
 
   TEST(ID) {
@@ -62,14 +84,38 @@ class Interpret {
         case 4:
           this.#get(index);
           index += 2;
+          break;
+        case 5:
+          index = this.#jumpIfTrue(index);
+          break;
+        case 6:
+          index = this.#jumpIfFalse(index);
+          break;
+        case 7:
+          this.#isLessThan(index);
+          index += 4;
+          break;
+        case 8:
+          this.#isEquals(index);
+          index += 4;
+          break;
+        default:
+          throw new Error(`Unknown opcode: ${opCode} at position ${index}`);
       }
     }
   }
 }
+
 const main = (data) => {
   const intCode = new Interpret(data);
-  return intCode.TEST(1);
+  return intCode.TEST(5);
 };
 
-const data = Deno.readTextFileSync("./day-5/input.txt").split(",").map(Number);
+const data = Deno.readTextFileSync("./day-5/input.txt")
+  .trim()
+  .split(",")
+  .map(Number);
+
+console.time();
 main(data);
+console.timeEnd();
